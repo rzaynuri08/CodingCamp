704d364d32290b9d208a7937958029f6e103d3e4
rzaynuri08 <rzaynuri08@gmail.com>
2023-10-16 21:44:04 +0700

    add API Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("androidx.fragment:fragment:1.3.6") // Sesuaikan versi dengan versi yang Anda gunakan
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
