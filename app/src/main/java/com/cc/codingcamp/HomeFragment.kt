e09c6f6caeac8a88e86b2c513544c20d29911c41
rzaynuri08 <rzaynuri08@gmail.com>
2023-10-17 18:04:21 +0700

    Resolve fill on Recycleview

--- app/src/main/java/com/cc/codingcamp/HomeFragment.kt
+++ app/src/main/java/com/cc/codingcamp/HomeFragment.kt
@@ -6,13 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.cc.codingcamp.modal.ProductItem
 
 class HomeFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var productAdapter: ProductAdapter
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val productList = ArrayList<ProductItem>()
+        productList.add(ProductItem(R.drawable.card, "Belajar Konsep OOP Dalam Bahasa Java", "Rp. 120.000"))
+        productList.add(ProductItem(R.mipmap.ic_launcher, "Membuat ppp dengan Java", "Rp. 120.000"))
+        productList.add(ProductItem(R.mipmap.ic_launcher, "Membuat ppp dengan Java", "Rp. 120.000"))
+
+        recyclerView = view?.findViewById(R.id.recyclerView) as RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+
+        productAdapter = ProductAdapter(requireContext(), productList)
+        recyclerView.adapter = productAdapter
+
+        return view
     }
 }
