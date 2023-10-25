c3989af4271840f4918875e4869670f3f0289978
rzaynuri08 <rzaynuri08@gmail.com>
2023-10-26 02:36:38 +0700

    resolve push metode push to API

--- app/src/main/java/com/cc/codingcamp/restApi/Service.kt
+++ app/src/main/java/com/cc/codingcamp/restApi/Service.kt
@@ -1,19 +1,21 @@
 package com.test.learnactivity.API
 
 import com.cc.codingcamp.restApi.ApiService
+import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Service {
-    private const val BASE_URL = "https://apicodingcamp.000webhostapp.com/" // Ganti dengan URL basis API
+    private const val BASE_URL = "https://billy30.000webhostapp.com/projectWebS3/api/" // Ganti dengan URL basis API
 
     // Properti apiService yang bisa diakses dari luar objek Service
     val apiService: ApiService by lazy {
+        val gson = GsonBuilder().setLenient().create()
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson)) // Perbaiki bagian ini
             .build()
 
-        retrofit.create(ApiService::class.java)
-        }
+        retrofit.create(ApiService::class.java) // Perbaiki bagian ini
+    }
 }
