1d498d79e83f622161a85fd0db5da4e3ef6ec4e5
rzaynuri08 <rzaynuri08@gmail.com>
2023-10-26 02:34:56 +0700

    resolve push metode to API

--- app/src/main/java/com/cc/codingcamp/fragment/RegisterFragment.kt
+++ app/src/main/java/com/cc/codingcamp/fragment/RegisterFragment.kt
@@ -6,12 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.fragment.app.FragmentTransaction
+import android.widget.Toast
 import com.cc.codingcamp.R
+import com.cc.codingcamp.modal.ResponseModel
+import com.cc.codingcamp.modal.Register
+import com.google.android.material.textfield.TextInputEditText
+import com.test.learnactivity.API.Service
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 // TODO: Rename parameter arguments, choose names that match
 class RegisterFragment : Fragment() {
     private lateinit var btnMasuk: Button
+    private lateinit var btnDaftar: Button
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,16 +28,68 @@ class RegisterFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_register, container, false)
 
         btnMasuk = view.findViewById(R.id.btn_signin)
+        btnDaftar = view.findViewById(R.id.btn_register)
 
-        btnMasuk.setOnClickListener {
-            // Membuat instance RegisterFragment
-            val LoginFragment = LoginFragment()
+//        btnMasuk.setOnClickListener {
+//            // Membuat instance LoginFragment
+//            val loginFragment = LoginFragment()
+//
+//            // Memulai transaksi fragment untuk mengganti fragment
+//            val transaction = requireFragmentManager().beginTransaction()
+//            transaction.replace(R.id.fragment_layout, loginFragment) // Ganti dengan ID container Anda
+//            transaction.commit()
+//        }
 
-            // Memulai transaksi fragment untuk mengganti fragment
-            val transaction: FragmentTransaction = requireFragmentManager().beginTransaction()
-            transaction.replace(R.id.fragment_layout, LoginFragment) // Ganti dengan ID container Anda
-            transaction.commit()
+        btnDaftar.setOnClickListener {
+            // Dapatkan data dari input
+            val username = view.findViewById<TextInputEditText>(R.id.txt_username).text.toString()
+            val namaLengkap = view.findViewById<TextInputEditText>(R.id.txt_name).text.toString()
+            val password = view.findViewById<TextInputEditText>(R.id.txt_password).text.toString()
+            val noHp = view.findViewById<TextInputEditText>(R.id.txt_nohp).text.toString()
+            val email = view.findViewById<TextInputEditText>(R.id.txt_email).text.toString()
+
+
+            // Membuat objek Register
+            val register = Register(username, namaLengkap, password, noHp, email)
+
+            // Memanggil fungsi untuk mendaftar (register)
+            registerUser(register)
         }
+
         return view
+    }
+
+    private fun registerUser(register: Register) {
+        val apiService = Service.apiService
+        val call = apiService.registerUser(register)
+
+        call.enqueue(object : Callback<ResponseModel> {
+            override fun onResponse(call: Call<ResponseModel>, response: Response<ResponseModel>) {
+                if (response.isSuccessful) {
+                    val responseModel = response.body()
+                    if (responseModel != null) {
+                        // Registrasi berhasil
+                        showSuccessToast(responseModel.message)
+                    }
+                } else {
+                    // Registrasi gagal
+                    showErrorToast("Registrasi gagal. Cek kembali data Anda.")
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseModel>, t: Throwable) {
+                // Error pada koneksi atau server
+                showErrorToast("Username sudah digunakan")
+//                showErrorToast("$register")
+            }
+        })
+    }
+
+    private fun showSuccessToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showErrorToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/cc/codingcamp/restApi/ApiService.kt
+++ app/src/main/java/com/cc/codingcamp/restApi/ApiService.kt
@@ -1,12 +1,14 @@
 package com.cc.codingcamp.restApi
 
 import com.cc.codingcamp.modal.Event
+import com.cc.codingcamp.modal.Register
 import com.cc.codingcamp.modal.ResponseModel
 import retrofit2.Call
 import retrofit2.http.GET
 import com.cc.codingcamp.modal.User
 import retrofit2.http.POST
 import retrofit2.http.Body
+import retrofit2.http.FormUrlEncoded
 
 interface ApiService {
     // Definisikan endpoint untuk mengambil daftar pengguna
@@ -16,6 +18,6 @@ interface ApiService {
     @GET("dataevent.php") // Ganti dengan URL sesuai dengan API Anda
     fun getEvent(): Call<List<Event>>
 
-    @POST("register.php") // Ganti dengan endpoint yang sesuai
-    fun registerUser(@Body user: User): Call<ResponseModel>
+    @POST("register.php")
+    fun registerUser(@Body user: Register): Call<ResponseModel>
 }
