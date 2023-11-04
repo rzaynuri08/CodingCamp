21826106dad4a21ab2d083a1a7aeb054a5697471
rzaynuri08 <rzaynuri08@gmail.com>
2023-11-04 19:30:31 +0700

    add : modal for data Course

---
+++ app/src/main/java/com/cc/codingcamp/modal/Course.kt
@@ -0,0 +1,10 @@
+package com.cc.codingcamp.modal
+
+data class Course(
+    val id_modul : String,
+    val judul : String,
+    val keterangan : String,
+    val gambar : String,
+    val tujuan : String,
+    val harga : Int,
+    val id_jenismodul : String)
